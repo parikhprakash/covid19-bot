f1c2c7f1f5794602dc6fdc66a6c71f848b9f783e
parikhprakash <parikhprakash@gmail.com>
2020-05-17 12:19:12 +0530

    Docker Update

--- actions/actions.py
+++ actions/actions.py
@@ -16,9 +16,29 @@ from rasa_sdk.executor import CollectingDispatcher
 import pickle
 
 from dateutil import relativedelta, parser
+try:
+    with open('daily_data.pkl','rb') as fp:
+        df = pickle.load(fp)
+except:
+    import pandas as pd
+    import requests
 
-with open('daily_data.pkl','rb') as fp:
-    df = pickle.load(fp)
+    all_data_response = requests.get('https://api.covid19india.org/data.json')
+    all_data = all_data_response.json()
+    daily_data = all_data['cases_time_series']
+
+    df = pd.DataFrame(daily_data)
+    df['date'] = df['date'].apply(lambda x: x + '2020')
+    df['date'] = pd.to_datetime(df['date'],format="%d %B %Y")
+    for col in df.columns:
+        if col != 'date':
+            df[col] = pd.to_numeric(df[col])
+
+    df['dailyactive'] = df['dailyconfirmed'] - df['dailyrecovered'] - df['dailydeceased']
+    df['totalactive'] = df['totalconfirmed'] - df['totalrecovered'] - df['totaldeceased']
+    import pickle
+    with open('daily_data.pkl','wb') as fp:
+        pickle.dump(df,fp)
 
 def format_time_by_grain(time, grain=None):
     grain_format = {
